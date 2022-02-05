af021471d77c13436d9ac327673a8fc8de697ac9
UsadaPekora <usada@pekora.dev>
2022-02-06 02:11:30 +0900

    test

--- ec2.tf
+++ ec2.tf
@@ -7,13 +7,3 @@ resource "aws_instance" "backstage_server" {
     Name = "UsakenBackstageEC2"
   }
 }
-
-resource "aws_instance" "test_server" {
-  ami           = "ami-0454bb2fefc7de534"
-  instance_type = "t2.micro"
-  key_name = "pekora-main-key"
-
-  tags = {
-    Name = "UsakenBackstageEC2"
-  }
-}
