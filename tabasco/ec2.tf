41962ffa6d3d2ecf013664998bbb4b2a9848dc32
UsadaPekora <usada@pekora.dev>
2022-02-05 22:09:05 +0900

    backstage setting

---
+++ tabasco/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "app_server" {
+  ami           = "ami-0454bb2fefc7de534"
+  instance_type = "t2.micro"
+  key_name = "pekora-main-key"
+
+  tags = {
+    Name = "TabascoBackstageServer"
+  }
+}
