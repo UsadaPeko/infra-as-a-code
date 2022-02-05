5edd6b7571c3aea7623e60a23d596128324fc528
UsadaPekora <usada@pekora.dev>
2022-02-05 23:09:19 +0900

    test

--- tabasco/ec2.tf
+++ tabasco/ec2.tf
@@ -1,29 +1,9 @@
-resource "aws_instance" "app_server" {
+resource "aws_instance" "backstage_server" {
   ami           = "ami-0454bb2fefc7de534"
   instance_type = "t2.micro"
   key_name = "pekora-main-key"
 
   tags = {
-    Name = "TabascoBackstageServer"
-  }
-}
-
-resource "aws_instance" "test" {
-  ami           = "ami-0454bb2fefc7de534"
-  instance_type = "t2.micro"
-  key_name = "pekora-main-key"
-
-  tags = {
-    Name = "TestServer"
-  }
-}
-
-resource "aws_instance" "test1" {
-  ami           = "ami-0454bb2fefc7de534"
-  instance_type = "t2.micro"
-  key_name = "pekora-main-key"
-
-  tags = {
-    Name = "TestServer"
+    Name = "UsakenBackstageServer"
   }
 }
