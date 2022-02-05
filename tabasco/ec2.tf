72634b4853caa1fef01dc6fec45b30468ad20600
UsadaPekora <usada@pekora.dev>
2022-02-06 00:47:40 +0900

    test

--- tabasco/ec2.tf
+++ tabasco/ec2.tf
@@ -7,13 +7,3 @@ resource "aws_instance" "backstage_server" {
     Name = "UsakenBackstageServer"
   }
 }
-
-resource "aws_instance" "test" {
-  ami           = "ami-0454bb2fefc7de534"
-  instance_type = "t2.micro"
-  key_name = "pekora-main-key"
-
-  tags = {
-    Name = "test1"
-  }
-}
